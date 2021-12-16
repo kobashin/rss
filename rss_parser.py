dc5f93918cf6a0f471c3c2f5421872d23f014b74
genuine <shin5884genuine@outlook.com>
2021-12-17 01:52:41 +0900

    fix : minor fix

--- rss_parser.py
+++ rss_parser.py
@@ -2,6 +2,7 @@ import sys
 import pprint
 pprint.pprint(sys.path)
 sys.path.append('/home/ec2-user/.local/lib/python3.7/site-packages')
+pprint.pprint(sys.path)
 
 import feedparser
 import pandas as pd
